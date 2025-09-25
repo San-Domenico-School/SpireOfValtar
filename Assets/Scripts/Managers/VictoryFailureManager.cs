613733cc37744bbbda704a18253da5989f3c6f79
SebastianSD27 <143214791+SebastianSD27@users.noreply.github.com>
2025-09-25 14:04:58 -0700

    Finished

---
+++ Assets/Scripts/Managers/VictoryFailureManager.cs
@@ -0,0 +1,135 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/******************************************************************************
+ * Manages victory and failure systems for player -- death or defeating all enemies
+ * 
+ *              TO-DO
+ * 3. cross check fields and names
+ * 
+ * Sebastian Balakier
+ * Version 1.0, 9/15/2025
+ *****************************************************************************/
+
+public class VictoryFailureManager : MonoBehaviour
+{
+    [SerializeField] GameObject Door;
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private GameObject victoryUI;
+    public int playerHealth = 100;
+    public int enemyCount = 3;
+    private int lastLoggedHealth;
+    public static VictoryFailureManager Instance;
+    private bool isGameOver = false;
+    private bool isVictory = false;
+    //TESTING FIELDS
+    //private float enemyDecreaseTimer = 0f;
+    //private float enemyDecreaseInterval = 1f;
+    //private float healthDecreaseTimer = 0f;
+    //private float healthDecreaseInterval = 1f;
+
+    private void Start()
+    {
+        GameObject.Find("Door").tag = "Locked"; // door starts locked
+        playerHealth = 100;
+        lastLoggedHealth = playerHealth;
+        Debug.Log("Start health:" + playerHealth);
+        enemyCount = 3;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        if (victoryUI) victoryUI.SetActive(false);
+        if (gameOverUI) gameOverUI.SetActive(false);
+
+    }
+
+    private void Update() // called every frame
+    {
+        // TEST GAME OVER
+        /*
+        healthDecreaseTimer += Time.deltaTime;
+        if (healthDecreaseTimer >= healthDecreaseInterval)
+        {
+            healthDecreaseTimer = 0f;
+            playerHealth -= 10;
+            Debug.Log("Player health =" + playerHealth);
+        */
+            if (playerHealth <= 0)
+            {
+                isGameOver = true;
+                GameOver();
+            }
+
+            if (playerHealth != lastLoggedHealth)
+            {
+                Debug.Log("Player health = " + playerHealth);
+                lastLoggedHealth = playerHealth;
+            }
+
+            // TEST VICTORY
+            /*
+            enemyDecreaseTimer += Time.deltaTime;
+            if (enemyDecreaseTimer >= enemyDecreaseInterval)
+            {
+                enemyDecreaseTimer = 0f;
+                enemyCount -= 1;
+            }
+            */
+            if (enemyCount <= 0)
+            {
+                isVictory = true;
+                LevelComplete();
+            }
+
+            void GameOver() // level lost
+            {
+                if (!isGameOver == false)
+                {
+                    isGameOver = true;
+                    if (gameOverUI == false)
+                    {
+                        gameOverUI.SetActive(true);
+                    }
+                    gameOverUI.SetActive(true);
+                    Time.timeScale = 0f;
+
+                    Debug.Log("Game Over");
+                }
+            }
+
+            void LevelComplete() // level completed
+            {
+                if (!isVictory == false)
+                {
+                    isVictory = true;
+
+                    if (isVictory == true)
+                    {
+                        Door.tag = "Completed";
+                    }
+                    if (victoryUI == false)
+                    {
+                        victoryUI.SetActive(true);
+                    }
+                    victoryUI.SetActive(true);
+                    if (Door.CompareTag("Completed"))
+                    {
+                        Debug.Log("Door open");
+                    }
+
+                    Destroy(Door);
+                }
+                else
+                {
+                    Debug.Log("Door is not responding");
+                }
+            }
+        }
+    }
+}
