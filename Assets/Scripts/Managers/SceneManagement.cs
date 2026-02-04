003eb6906e1d02b858101fefca93042dc0f8e94b
GlebSD <hkucherenko25@sandomenico.org>
2026-02-04 14:17:37 -0800

    Spawn FIX

--- Assets/Scripts/Managers/SceneManagement.cs
+++ Assets/Scripts/Managers/SceneManagement.cs
@@ -1,43 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class SceneManagement : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Player progressed to next scene: " + sceneToLoad);
             SceneManager.LoadScene(sceneToLoad);
-            Debug.Log("Player progressed to next scene");
-        }
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        Debug.Log("is this method running?");
-        GameObject spawnPoint = GameObject.Find("Spawnpoint");
-
-        if (spawnPoint != null)
-        {
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
-            player.transform.position = spawnPoint.transform.position;
-            Debug.Log("Player put to spawnpoint");
-        }
-        else
-        {
-            Debug.LogWarning("Spawnpoint not found in scene!");
         }
     }
 }
