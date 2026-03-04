c9972c75578f0f3b9e084d8c50e532e2f8d5d00d
GlebSD <hkucherenko25@sandomenico.org>
2026-03-04 13:59:39 -0800

    Revert "Gold works"
    This reverts commit 12649338c0f930305e2e9c21898b9c8e3a7197c0.

--- Assets/Scripts/Player/GoldCollector.cs
+++ Assets/Scripts/Player/GoldCollector.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
-
+/* Sebastian 2/24/26
+ * This script is used to collect gold.
+ * It is a singleton and will persist across scenes.
+ * It will destroy the gold object when it is collected.
+ * It will add the gold to the player's gold count.
+ * It will update the gold count in the UI.
+ * It will save the gold count to the player's data.
+ * It will load the gold count from the player's data.
+ */
 public class GoldCollector : MonoBehaviour
 {
     public static GoldCollector Instance;
@@ -8,15 +16,15 @@ public class GoldCollector : MonoBehaviour
 
     private void Awake()
     {
-       
+        // Singleton check
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); 
+            DontDestroyOnLoad(gameObject); // Persist across scenes 
         }
         else
         {
-            Destroy(gameObject); 
+            Destroy(gameObject); // Prevent duplicates
         }
     }
 
@@ -36,13 +44,7 @@ public class GoldCollector : MonoBehaviour
         return goldCollected;
     }
 
-    public void AddGold(int amount)
-    {
-        goldCollected += amount;
-        Debug.Log("Gold Collected: " + goldCollected);
-    }
-
-    
+    // Returns true and deducts the amount if the player can afford it.
     public bool SpendGold(int amount)
     {
         if (goldCollected < amount) return false;
