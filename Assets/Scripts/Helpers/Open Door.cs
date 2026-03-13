d5a6c1937ad1f81ecbc29f3fe1a590f6b0f57787
Seamus <scrain26@sandomenico.org>
2026-03-13 14:15:55 -0700

    import tower and modified terrain

---
+++ Assets/Scripts/Helpers/Open Door.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+/************************************
+ * Opens the door to the great hall from the outside 
+ * and controls when audio plays 
+ * Seamus
+ * Version 1.0
+ ************************************/
+public class OpenDoor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
