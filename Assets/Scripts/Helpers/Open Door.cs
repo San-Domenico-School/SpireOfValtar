f6f8311e75d613bae6d87c18521fbf59cc83772d
Seamus <scrain26@sandomenico.org>
2026-03-13 14:48:39 -0700

    make the door open when you walk up to it and plays sound (not tested)

--- Assets/Scripts/Helpers/Open Door.cs
+++ Assets/Scripts/Helpers/Open Door.cs
@@ -1,21 +1,36 @@
+using Unity.VisualScripting;
 using UnityEngine;
+
 /************************************
- * Opens the door to the great hall from the outside 
- * and controls when audio plays 
- * Seamus
- * Version 1.0
- ************************************/
+* Opens the door to the great hall from the outside 
+* and controls when audio plays 
+* Seamus
+* Version 1.0
+************************************/
+
 public class OpenDoor : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] AudioSource audioSource;
+    private bool first; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator.speed = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other) 
     {
-        
+        if (first)
+            return;
+
+        if (other.CompareTag("Player"))
+        {
+            animator.speed = 1;
+            audioSource.Play();
+            first = true;
+        }
     }
 }
